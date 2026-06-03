4c58feb3795cc766a11512903e6312c546cdd025
starry7827 <pydalal7@gmail.com>
2026-06-02 20:20:48 -0400

    fixed cup dragging, added glowing outline for cup button

--- Assets/Scripts/CupDragging.cs
+++ Assets/Scripts/CupDragging.cs
@@ -1,36 +1,145 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class CupDragging : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    private CanvasGroup canvasGroup;
-    private Transform originalParent;
-    public Transform trans;
-    float x;
-    float y;
-    Vector3 originalPosition;
+    public GameObject cupDragLeft;
+    public GameObject cupDragRight;
 
-    private void Awake() {
-        canvasGroup = GetComponent<CanvasGroup>();
+    public Sprite hoverImg;
+    public Sprite dropImg;
+    public Vector2 dropResultSize;
+
+    private Sprite originalDragSprite;
+    private Sprite originalHoverTargetSprite;
+
+    private GameObject currentlyHovered;
+
+    private Canvas canvas;
+    private GraphicRaycaster raycaster;
+
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = GetComponent<RectTransform>().position;
+
+        originalDragSprite = GetComponent<Image>().sprite;
+
+        canvas = GetComponentInParent<Canvas>();
+        raycaster = canvas.GetComponent<GraphicRaycaster>();
     }
 
-    void Start() {
-        x = transform.position.x;
-        y = transform.position.y;
-        originalPosition = new Vector3(x, y, 0f);
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Image img = GetComponent<Image>();
+        img.color = new Color(img.color.r, img.color.g, img.color.b, 1f);
+        img.raycastTarget = false;
     }
 
-    public void OnBeginDrag(PointerEventData eventData) {
-        originalParent = transform.parent;
-        canvasGroup.blocksRaycasts = false;
+    public void OnDrag(PointerEventData eventData)
+    {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+
+        Vector2 localPoint;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            canvas.transform as RectTransform,
+            eventData.position,
+            canvas.worldCamera,
+            out localPoint
+        );
+
+        rectTransform.localPosition = localPoint;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        raycaster.Raycast(eventData, results);
+
+        GameObject newHover = null;
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject == cupDragLeft)
+            {
+                CupDropSlot slot = cupDragLeft.GetComponent<CupDropSlot>();
+
+                if (!slot.occupied)
+                {
+                    newHover = cupDragLeft;
+                }
+
+                break;
+            }
+
+            if (result.gameObject == cupDragRight)
+            {
+                CupDropSlot slot = cupDragRight.GetComponent<CupDropSlot>();
+
+                if (!slot.occupied)
+                {
+                    newHover = cupDragRight;
+                }
+
+                break;
+            }
+        }
+
+        if (newHover != currentlyHovered)
+        {
+            Image dragImage = GetComponent<Image>();
+            if (currentlyHovered != null)
+            {
+                CupDropSlot slot = currentlyHovered.GetComponent<CupDropSlot>();
+
+                if (!slot.occupied)
+                {
+                    Image prevImage = currentlyHovered.GetComponent<Image>();
+
+                    prevImage.sprite = originalHoverTargetSprite;
+
+                    prevImage.color = new Color(prevImage.color.r, prevImage.color.g, prevImage.color.b, 0f);
+                    dragImage.color = new Color( dragImage.color.r, dragImage.color.g, dragImage.color.b, 1f);
+                }
+            }
+
+            if (newHover != null)
+            {
+                Image targetImage = newHover.GetComponent<Image>();
+
+                originalHoverTargetSprite = targetImage.sprite;
+
+                targetImage.color = new Color(targetImage.color.r, targetImage.color.g, targetImage.color.b, 1f);
+
+                if (hoverImg != null)
+                    targetImage.sprite = hoverImg;
+                    dragImage.color = new Color( dragImage.color.r, dragImage.color.g, dragImage.color.b, 0f);
+            }
+            currentlyHovered = newHover;
+        }
     }
 
-    public void OnDrag(PointerEventData eventData) {
-        transform.position = eventData.position;
-    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Image dragImage = GetComponent<Image>();
 
-    public void OnEndDrag(PointerEventData eventData) {
-        canvasGroup.blocksRaycasts = true;
-        trans.position = originalPosition;
+        if (currentlyHovered != null)
+        {
+            CupDropSlot slot = currentlyHovered.GetComponent<CupDropSlot>();
+            if (!slot.occupied)
+            {
+                Image targetImage = currentlyHovered.GetComponent<Image>();
+                if (dropImg != null)
+                    targetImage.sprite = dropImg;
+                targetImage.color = new Color(targetImage.color.r, targetImage.color.g, targetImage.color.b, 1f);
+                slot.occupied = true;
+            }
+        }
+
+        dragImage.color = new Color( dragImage.color.r, dragImage.color.g, dragImage.color.b, 0f);
+        dragImage.raycastTarget = true;
+        GetComponent<RectTransform>().position = originalPosition;
+        currentlyHovered = null;
     }
 }
